d8a6290957d442aa0ed9de166f2653dc338b1695
Shikigeru <lunyov.yu@gmail.com>
2020-02-23 15:30:41 +0200

    History and rightinfo languages rework

--- src/components/RightInfoBlock.js
+++ src/components/RightInfoBlock.js
@@ -4,31 +4,25 @@ import '../styles/RightInfoBlock.sass';
 
 const RightInfoBlock = (props) => {
     const {lang} = props;
-    if (lang === 'us') {
-        return (
-            <div className="right-info">
-                <a href="#/blog/5" className="main-link">
-                    <h2 className="medium-title">Carnabands Show 2019</h2>
-                    <img src="/images/carnabands.jpg" alt="carnabands" />
-                </a>  
-                <p>
-                    February 23, 24, 2019 the orchestra of the 8th Chernihiv Training Center of the State Special Service of the Ministry of Defense of Ukraine participated in the 99th international festival "Carnaband's Show 2019" in the city of Chalon-sur-Seine, Republic of France.
-                </p>
-                <a href="#/blog/5" className="main-link">READ MORE</a>
-            </div>
-        )
+    const contentText = {
+        titleText: 'Carnabands Show 2019',
+        maintText: (lang === 'us' ? 
+                        'February 23, 24, 2019 the orchestra of the 8th Chernihiv Training Center of the State Special Service of the Ministry of Defense of Ukraine participated in the 99th international festival "Carnaband\'s Show 2019" in the city of Chalon-sur-Seine, Republic of France.'
+                        :
+                        '23, 24 лютого 2019 року оркестр 8-го Чернігівського навчального центру державної спеціальної служби Міністерства оборони України приймав участь в 99 міжнародному фестивалі «Carnaband\’s Show 2019» в місті Шалон Сюр-Сон республіки Франція.'    
+                    ),
+        buttonText: lang === 'us' ? 'READ MORE' : 'Детальніше'
     }
     return (
         <div className="right-info">
             <a href="#/blog/5" className="main-link">
-                <h2 className="medium-title">Carnabands Show 2019</h2>
-                <img src="/images/carnabands.jpg" alt="carnabands"/>
-            </a>            
+                <h2 className="medium-title">{contentText.titleText}</h2>
+                <img src="/images/carnabands.jpg" alt="carnabands" />
+            </a>
             <p>
-                23, 24 лютого 2019 року оркестр 8-го Чернігівського навчального центру державної спеціальної служби Міністерства оборони України приймав участь в 99 міжнародному фестивалі «Carnaband’s
-                Show 2019» в місті Шалон Сюр-Сон республіки Франція.
+                {contentText.maintText}
             </p>
-            <a href="#/blog/5" className="main-link">Детальніше</a>
+            <a href="#/blog/5" className="main-link">{contentText.buttonText}</a>
         </div>
     )
 }
