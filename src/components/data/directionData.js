777f4b5831d2d292ffc4654ef426e0adde1267fc
Shikigeru <lunyov.yu@gmail.com>
2020-02-24 09:33:30 +0200

    direction changes

--- src/components/data/directionData.js
+++ src/components/data/directionData.js
@@ -4,34 +4,43 @@ const directionData = [
         post: 'НАЧАЛЬНИК ОРКЕСТРУ - ВІЙСЬКОВИЙ ДИРИГЕНТ',
         rank: 'полковник',
         name: 'Смаль Микола Валентинович',
-        tel: '+380 050 959 88 02',
+        tel: '+380 50 959 88 02',
         email: 'mykolasmal150579@gmail.com',
         picture: '/images/direction/smal.jpg'
     },
     {
         id: 2,
-        post: 'ГОЛОВНИЙ СТАРШИНА ОРКЕСТРУ',
-        rank: 'прапорщик',
-        name: 'Срібний Андрій Вікторович',
-        tel: '+380 063 425 73 27',
-        email: '',
-        picture: '/images/direction/ker6.jpg'
+        post: 'ЗАСТУПНИК НАЧАЛЬНИКА ОРКЕСТРУ',
+        rank: 'майор',
+        name: 'Школовий Борис Борисович',
+        tel: '+380 63 060 57 14',
+        email: 'bbshkol7244@gmail.com',
+        picture: '/images/direction/shkoloviy.jpg'
     },
     {
         id: 3,
-        post: 'АРТИСТ ВИЩОЇ КАТЕГОРІЇ',
-        rank: 'старший прапорщик',
-        name: 'Чепурний Анатолій Федорович',
-        tel: '+380 093 278 61 49',
+        post: 'ГОЛОВНИЙ СТАРШИНА ОРКЕСТРУ',
+        rank: 'прапорщик',
+        name: 'Срібний Андрій Вікторович',
+        tel: '+380 63 425 73 27',
         email: '',
-        picture: '/images/direction/ker41.jpg'
+        picture: '/images/direction/ker6.jpg'
     },
     {
         id: 4,
         post: 'АРТИСТ ВИЩОЇ КАТЕГОРІЇ',
         rank: 'старший прапорщик',
+        name: 'Чепурний Анатолій Федорович',
+        tel: '+380 93 278 61 49',
+        email: '',
+        picture: '/images/direction/ker41.jpg'
+    },
+    {
+        id: 5,
+        post: 'АРТИСТ ВИЩОЇ КАТЕГОРІЇ',
+        rank: 'старший прапорщик',
         name: "Остроух В'ячеслав Миколайович",
-        tel: '+380 067 276 15 88',
+        tel: '+380 67 276 15 88',
         email: '',
         picture: '/images/direction/ker5.jpg'
     }
--- src/components/data/directionDataUS.js
+++ src/components/data/directionDataUS.js
@@ -4,16 +4,25 @@ const directionDataUS = [
         post: 'CHIEF ORCHESTRA - MILITARY CONDUCTOR',
         rank: 'colonel',
         name: 'Smal Nikolay Valentinovich',
-        tel: '+380 050 959 88 02',
+        tel: '+380 50 959 88 02',
         email: 'mykolasmal150579@gmail.com',
         picture: '/images/direction/smal.jpg'
+    },
+    {
+        id: 2,
+        post: 'CHIEF ORCHESTRA - MILITARY CONDUCTOR',
+        rank: 'major',
+        name: 'Shkoloviy Boris Borisovich',
+        tel: '+380 63 060 57 14',
+        email: 'bbshkol7244@gmail.com',
+        picture: '/images/direction/shkoloviy.jpg'
     },
     {
         id: 2,
         post: 'MAIN FIRST SERGEANT OF ORCHESTRA',
         rank: 'ensign',
         name: 'Sribniy Andriy Victorovich',
-        tel: '+380 063 425 73 27',
+        tel: '+380 63 425 73 27',
         email: '',
         picture: '/images/direction/ker6.jpg'
     },
@@ -22,7 +31,7 @@ const directionDataUS = [
         post: 'ARTIST OF HIGHEST CATEGORY',
         rank: 'senior warrant',
         name: 'Chepurny Anatoly Fedorovich',
-        tel: '+380 093 278 61 49',
+        tel: '+380 93 278 61 49',
         email: '',
         picture: '/images/direction/ker41.jpg'
     },
@@ -31,7 +40,7 @@ const directionDataUS = [
         post: 'ARTIST OF HIGHEST CATEGORY',
         rank: 'senior warrant',
         name: "Ostrouh Vyacheslav Mykolayovych",
-        tel: '+380 067 276 15 88',
+        tel: '+380 67 276 15 88',
         email: '',
         picture: '/images/direction/ker5.jpg'
     }
