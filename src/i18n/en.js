a80ff90a823670fa9cae2c2e6e6c6c1aa8dd86a8
Shikigeru <lunyov.yu@gmail.com>
2020-09-29 11:11:00 +0300

    i18n integration

---
+++ src/i18n/en.js
@@ -0,0 +1,22 @@
+export default {
+  Main: {
+    descriptionFirstParagraph:
+      'Without exaggeration, it is safe to call our military orchestra' +
+      ' of the 8th Chernihiv Learning Center a calling card of the State' +
+      ' Special Transport Service.',
+    descriptionSecondParagraph:
+      'Military orchestra started his creative biography from the first' +
+      ' days of the existence of military base. Born in 1950, the team began' +
+      ' a great creative career, honorably fulfilling the responsible tasks' +
+      ' of cultural service and aesthetic education of servicemen.',
+    descriptionThirdParagraph:
+      'Since the beginning of the formation and for today, the orchestra' +
+      ' has led by 8 military conductors. The creative team are well known' +
+      ' not only in Chernihiv, but also far beyond its borders. Almost all' +
+      ' state events and military ceremonies of our state do not pass' +
+      ' without bright performances of our collective. With its highly' +
+      ' artistic inputs to the Ukrainian audience, as well as on the scenes' +
+      ' of the states of the world, the collective gained recognition of the' +
+      ' demanding audience of the international community.',
+  },
+};
