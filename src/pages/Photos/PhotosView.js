1e1b9914cb4936f2254e1ffa85d2daa58afbac1d
Shikigeru <lunyov.yu@gmail.com>
2020-09-30 17:45:49 +0300

    fixes

--- src/pages/Photos/PhotosView.js
+++ src/pages/Photos/PhotosView.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
 import MainLayout from '../../layouts/MainLayout';
 
 const PhotosView = ({ sliderSettings }) => {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    document.title = t('Menu.photos');
+  });
+
   return (
     <MainLayout>
       <Slider {...sliderSettings} className="main__slider">
